e9c0685e88412b987a829de76db9ed331e46f449
Alekhd <90794297+Alikohd@users.noreply.github.com>
2024-08-31 05:15:09 +0300

    init

---
+++ src/main/java/ru/oleevych/cloudspace/CloudSpaceApplication.java
@@ -0,0 +1,13 @@
+package ru.oleevych.cloudspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudSpaceApplication.class, args);
+    }
+
+}
