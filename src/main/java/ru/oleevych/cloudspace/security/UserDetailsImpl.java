d788d79754969cc97d825bfa15ebf638fa36288f
Alekhd <90794297+Alikohd@users.noreply.github.com>
2024-09-01 20:45:57 +0300

    feat: add base Spring Security config

---
+++ src/main/java/ru/oleevych/cloudspace/security/UserDetailsImpl.java
@@ -0,0 +1,31 @@
+package ru.oleevych.cloudspace.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.oleevych.cloudspace.entity.User;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
